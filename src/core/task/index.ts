b43a62615f210ca2aeb247405f29353c6f46658d
Sooah Lee <sooahlee01@gmail.com>
2025-07-03 17:52:50 +0900

    Replace system prompt to planning in api request for planning (#63)
    * fix: replace system prompt to planning for planning
    * fix: fix getting stream

--- src/core/task/index.ts
+++ src/core/task/index.ts
@@ -1061,11 +1061,8 @@ export class Task {
 							this.sidebarController.phaseTracker.getProjectOverview(),
 						)
 					: (task ?? "")
-			if (this.taskState.isPhaseRoot) {
-				userContent = [{ type: "text", text: `${PROMPTS.PLANNING}\n\n<task>\n${task}\n</task>` }, ...imageBlocks]
-			} else {
-				userContent = [{ type: "text", text: `<task>\n${phaseAwarePrompt}\n</task>` }, ...imageBlocks]
-			}
+
+			userContent = [{ type: "text", text: `<task>\n${phaseAwarePrompt}\n</task>` }, ...imageBlocks]
 		} else {
 			userContent = [{ type: "text", text: `<task>\n${task}\n</task>` }, ...imageBlocks]
 		}
@@ -2026,6 +2023,8 @@ export class Task {
 
 		const supportsBrowserUse = modelSupportsBrowserUse && !disableBrowserTool // only enable browser use if the model supports it and the user hasn't disabled it
 
+		const apiToUse = this.taskState.isPhaseRoot && forceModel ? this.createTemporaryApiHandler(forceModel) : this.api
+
 		const preferredLanguage = getLanguageKey(this.preferredLanguage as LanguageDisplay)
 		const preferredLanguageInstructions =
 			preferredLanguage && preferredLanguage !== DEFAULT_LANGUAGE_SETTINGS
@@ -2085,13 +2084,10 @@ export class Task {
 			// saves task history item which we use to keep track of conversation history deleted range
 		}
 		// Use forced model if specified, otherwise use default api
-		let stream
-		if (this.taskState.isPhaseRoot) {
-			const apiToUse = forceModel ? this.createTemporaryApiHandler(forceModel) : this.api
-			stream = apiToUse.createMessage(systemPrompt, contextManagementMetadata.truncatedConversationHistory)
-		} else {
-			stream = this.api.createMessage(systemPrompt, contextManagementMetadata.truncatedConversationHistory)
-		}
+		const stream = apiToUse.createMessage(
+			this.taskState.isPhaseRoot ? PROMPTS.PLANNING : systemPrompt,
+			contextManagementMetadata.truncatedConversationHistory,
+		)
 
 		const iterator = stream[Symbol.asyncIterator]()
 
